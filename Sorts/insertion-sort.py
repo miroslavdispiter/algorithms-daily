c4ffc701f3d445747f15645eb93a99bc2dced401
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-04-17 21:40:31 +0200

    Insertion Sort algoritam

--- Sorts/insertion-sort.py
+++ Sorts/insertion-sort.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+def insertionSort(nums):
+    for i in range(1, len(nums)):
+        j = i
+        while nums[j-1] > nums[j] and j > 0:
+            nums[j-1], nums[j] = nums[j], nums[j - 1]
+            j -= 1
+    return nums
+
+arr = [2, 6, 5, 1, 3, 4]
+print(insertionSort(arr))
