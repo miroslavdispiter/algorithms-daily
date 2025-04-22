1995a663aed076a08d303dbdb39ae9acad71a97c
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-04-22 17:26:26 +0200

    first-last-pos - binary search

--- Arrays/first-last-pos.py
+++ Arrays/first-last-pos.py
@@ -4,9 +4,8 @@ find the starting and ending position of a given target value.
 If target is not found in the array, return [-1, -1].
 You must write an algorithm with O(log n) runtime complexity.
 """
-# Made with time complexity O(n), tommorow I will try to make with
-# time complexity O(log n). Idea: Binary search
-
+# Made with time complexity O(n), linear search.
+"""
 def posFirstLast(nums, target):
     pos1 = -1
     pos2 = -1
@@ -19,10 +18,40 @@ def posFirstLast(nums, target):
         elif nums[i] == target and counter == 1:
             pos2 = i
     return [pos1, pos2]
+"""
+# Made with time complexity O(log(n)), binary search.
 
-nums = [5,7,7,8,8,8,8,10]
+def binarySearch(nums, target, findFirst):
+    left = 0
+    right = len(nums)-1
+    result = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            result = mid
+
+            if findFirst:
+                right = mid - 1  # nastavljamo traziti levo
+            else:
+                left = mid + 1  # nastavljamo traziti desno
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
+ 
+def posFirstLast(nums, target):
+    first = binarySearch(nums, target, True)
+    last = binarySearch(nums, target, False)
+    return [first, last]
+
+nums = [5,7,7,7,8,8,8,8,10]
 target = 7
 print(posFirstLast(nums, target))
+
 nums = []
 target = 0
 print(posFirstLast(nums, target))
